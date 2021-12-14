62fc26b6063ac5786d0e1a6f7846839782350dc7
Takumi Tomita <takumi.tomita.vf@gmail.com>
2021-12-14 20:06:01 +0900

    initial

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# aws-billing-notification-tools
+# aws-billing-notification-tool
+## Commands
+```
+export AWS_PROFILE=<your aws profile>
+
+#コマンドでWebhookを使場合は設定(option)
+export TEAMS_WEBHOOK_URL=<your teams webhook URL>
+
+#実行例
+% python app_shell.py   
+------------------------------------------------------
+11/01～11/29のクレジット適用後費用は、205.66 (USD)です。
+- AWS Config: 31.21
+- AWS Cost Explorer: 2.30
+- AWS Secrets Manager: 0.01
+- AWS Security Hub: 2.55
+- EC2 - Other: 115.99
+- Amazon Elastic Compute Cloud - Compute: 17.65
+- Amazon Elastic Container Service: 5.90
+- Amazon GuardDuty: 0.42
+- Amazon Relational Database Service: 8.07
+- Amazon Simple Storage Service: 0.61
+- AmazonCloudWatch: 1.93
+- Tax: 19.02
+------------------------------------------------------
+11/01～11/29のクレジット適用前費用は、431.12 (USD)です。
+- AWS Config: 31.21
+- AWS Cost Explorer: 5.25
+- AWS Secrets Manager: 0.01
+- AWS Security Hub: 3.12
+- EC2 - Other: 115.99
+- Amazon Elastic Compute Cloud - Compute: 73.81
+- Amazon Elastic Container Service: 5.90
+- Amazon GuardDuty: 0.42
+- Amazon Relational Database Service: 173.03
+- Amazon Simple Notification Service: 0.82
+- Amazon Simple Storage Service: 0.61
+- AmazonCloudWatch: 1.93
+- Tax: 19.02
+------------------------------------------------------
+```
+## Deploy Script
+AWS SAMを用いたビルド&デプロイスクリプト。
+
+```
+#!/bin/sh
+set -eu
+
+#aws-root
+AWS_PROFILE=root
+S3_BACKET=<your backet name>
+TEAMS_WEBHOOK_URL=<Teams Webhook URL>
+
+echo "Start sam build command."
+sam build
+
+echo "Start sam package command."
+sam package \
+    --output-template-file packaged.yaml \
+    --s3-bucket $S3_BACKET
+
+echo "Start sam deploy command."
+sam deploy \
+    --template-file packaged.yaml \
+    --stack-name NotifyBillingToTeams \
+    --capabilities CAPABILITY_IAM \
+    --parameter-overrides TeamsWebhookUrl=$TEAMS_WEBHOOK_URL
+```
+
+## Delete Stack
+```
+sam delete --stack-name NotifyBillingToTeams
+```
---
+++ aws_billing_app/app.py
@@ -0,0 +1,126 @@
+import os
+import json
+import boto3
+import requests
+from datetime import datetime, timedelta, date
+from typing import Union
+
+TEAMS_WEBHOOK_URL = os.environ['TEAMS_WEBHOOK_URL']
+
+#集計期間自動設定(今月初め〜本日)
+def get_date_range() -> Union[str,str]:
+    start_date = date.today().replace(day=1).isoformat()
+    end_date = date.today().isoformat()
+    if start_date == end_date:
+        end_of_month = datetime.strptime(start_date, '%Y-%m-%d') + timedelta(days=-1)
+        begin_of_month = end_of_month.replace(day=1)
+        return begin_of_month.date().isoformat(), end_date
+    return start_date, end_date
+(start_date, end_date) = get_date_range()
+
+#REST API(get_cost_and_usage)のパラメータ設定
+MY_PERIOD={
+    'Start': start_date,
+    'End': end_date
+}
+
+MY_GRANULARITY='MONTHLY'
+
+MY_FILTER={
+    'Not': {
+        'Dimensions': {
+            'Key': "RECORD_TYPE",
+            'Values': ["Credit"]
+        }
+    }
+}
+
+MY_METRIC='AmortizedCost'
+
+MY_GROUP_BY= {
+    'Type': 'DIMENSION',
+    'Key': 'SERVICE'
+}
+
+def lambda_handler(event, context) -> None:
+    client = boto3.client('ce', region_name='us-east-1')
+
+    start_day = (datetime.strptime(start_date, '%Y-%m-%d')).strftime('%m/%d')
+    end_yesterday = (datetime.strptime(end_date, '%Y-%m-%d') - timedelta(days=1)).strftime('%m/%d')
+
+    # クレジット適用後費用
+    total_cost_after_credit = round(float(get_total_cost(True, client)), 2)
+    title = f'{start_day} - {end_yesterday}のクレジット適用後費用は{total_cost_after_credit:.2f}(USD)です.'
+    services_cost_after_credit = get_services_msg(get_service_costs(True, client))
+    post_teams_webhook(title, '\n\n'.join(services_cost_after_credit))
+
+    # クレジット適用前費用
+    total_cost_before_credit = round(float(get_total_cost(False, client)), 2)
+    title = f'{start_day} - {end_yesterday}のクレジット適用前費用は{total_cost_before_credit:.2f}(USD)です.'
+    services_cost_before_credit = get_services_msg(get_service_costs(False, client))
+    post_teams_webhook(title, '\n\n'.join(services_cost_before_credit))
+
+def get_total_cost(inCredit:bool, client) -> str:
+    if inCredit:
+        response = client.get_cost_and_usage(
+            TimePeriod=MY_PERIOD,
+            Granularity=MY_GRANULARITY,
+            Metrics=[MY_METRIC]
+        )
+    else:
+        response = client.get_cost_and_usage(
+            TimePeriod=MY_PERIOD,
+            Granularity=MY_GRANULARITY,
+            Filter=MY_FILTER,
+            Metrics=[MY_METRIC]
+        )
+
+    return response['ResultsByTime'][0]['Total'][MY_METRIC]['Amount']
+    
+def get_service_costs(inCredit:bool, client) -> list:
+    if inCredit:
+        response = client.get_cost_and_usage(
+            TimePeriod=MY_PERIOD,
+            Granularity=MY_GRANULARITY,
+            Metrics=[MY_METRIC],
+            GroupBy=[MY_GROUP_BY]
+        )
+    else:
+        response = client.get_cost_and_usage(
+            TimePeriod=MY_PERIOD,
+            Granularity=MY_GRANULARITY,
+            Filter=MY_FILTER,
+            Metrics=[MY_METRIC],
+            GroupBy=[MY_GROUP_BY]
+        )
+
+    billings = []
+
+    for item in response['ResultsByTime'][0]['Groups']:
+        billings.append({
+            'service_name': item['Keys'][0],
+            'billing': item['Metrics'][MY_METRIC]['Amount']
+        })
+    return billings
+
+def get_services_msg(service_billings: list) -> dict:
+    servicess = []
+    for item in service_billings:
+        service_name = item['service_name']
+        billing = round(float(item['billing']), 2)
+        if billing == 0.0: continue
+        servicess.append(f'- {service_name}: {billing:.2f}')
+    return servicess
+
+def post_teams_webhook(title: str, services: str) -> None:
+    payload = {
+        "title": title,
+        "text": services
+    }
+
+    try:
+        response = requests.post(TEAMS_WEBHOOK_URL, data=json.dumps(payload))
+    except requests.exceptions.RequestException as e:
+        print(e)
+    else:
+        print("Teams Webhook Response(Status Code): " + str(response.status_code))
